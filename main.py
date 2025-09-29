93755061414e03e7f136c7104c757cc2abc67955
yang1024123 <yw10242020@163.com>
2025-09-29 10:18:09 +0800

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import pytest
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    pytest.main(['-s', '--clean-alluredir', '--alluredir=allure-results'])
-    os.system(r"allure generate -c -o allure-report")
